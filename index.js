430bac9c669fa58c3b4ea4cb64f80dae101a51b2
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-03-20 10:04:03 +0800

    seggregated the app individually

---
+++ index.js
@@ -0,0 +1,9 @@
+// stores our application logic
+var options = {
+    strings: ['Welcome to Project Destiny!', 'Ready to Explore?'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: false
+};
+
+var typed = new Typed('.typing', options);
